ad9e5b38faad405bd337370843c6f4d2e80dda45
aabidkhan0786 <khanaab786@gmail.com>
2021-08-22 16:56:01 +0530

    my travelling app

---
+++ src/components/Maps/Maps.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Maps = () => {
+    return (
+        <>
+            Maps
+        </>
+    )
+}
+
+export default Maps
---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PlaceList = () => {
+    return (
+        <>
+            PlaceList
+        </>
+    )
+}
+
+export default PlaceList
