939e25f54a40ac5ba9c985071ff4b1e3013a877b
aabidkhan0786 <khanaab786@gmail.com>
2021-08-27 23:59:40 +0530

    ui updated !!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 // import moduleName from './Head'
-import { Grid, CssBaseline} from "@material-ui/core"
+// import { Grid, CssBaseline} from "@material-ui/core"
 import Header from './components/Header/Header'
 // import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 import PlaceList from './components/PlaceList/PlaceList'
@@ -9,16 +9,13 @@ import Maps from './components/Maps/Maps'
 const App = () => {
     return (
         <>
-            <CssBaseline/>
-                <Header/>
-            <Grid container spacing ={3} style={{"width":"100%"}}>
-                <Grid item xs={12} md={4}>
-                    <PlaceList/>
-                </Grid>
-                <Grid item xs={12} md={8}>
-                    <Maps/>
-                </Grid>
-            </Grid>
+            <Header />
+            {/* <div className="container"> */}
+                <div class="row g-0">
+                    <div class="col-4 bg-dark">col-4</div>
+                    <div class="col-8">col-8</div>
+                </div>
+            {/* </div> */}
         </>
     )
 }
