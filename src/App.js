f53016c96afb0b48626d1918cf7044b7b9de6704
aabidkhan0786 <khanaab786@gmail.com>
2021-08-28 22:44:58 +0530

    list added !!

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Header from './components/Header/Header'
 // import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 import PlaceList from './components/PlaceList/PlaceList'
 import Maps from './components/Maps/Maps'
+import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 
 const App = () => {
     return (
         <>
             <Header />
-            {/* <div className="container"> */}
-                <div class="row g-0">
-                    <div class="col-4 bg-dark">col-4</div>
-                    <div class="col-8">col-8</div>
+            <div className="container-fluid">
+                <div class="row">
+                    <div className="col-lg-4 col-12"><PlaceDetails/></div>
+                    <div className="col-lg-8 col-12">col-8</div>
                 </div>
-            {/* </div> */}
+            </div>
         </>
     )
 }
--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -3,7 +3,13 @@ import React from 'react'
 const PlaceDetails = () => {
     return (
         <>
-            PlaceDetails
+            <div className="container-fluid">
+                    <h1>Maps & Foods:</h1>
+                <div class="row">
+                    <div className="col-lg-6 col-12">col-i</div>
+                    <div className="col-lg-6 col-12">col-v</div>
+                </div>
+            </div>
         </>
     )
 }
