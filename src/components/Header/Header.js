c00cfce4393da1ddf263c83591a2c6cbc28ddc01
aabidkhan0786 <khanaab786@gmail.com>
2021-08-29 23:11:33 +0530

    map function started !!

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
         <>
            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">aTraverzz</a>
+    <a class="navbar-brand" href="/">aTraverzz</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,9 +1,23 @@
+import  GoogleMapReact  from 'google-map-react'
 import React from 'react'
 
 const Maps = () => {
+    const coordinates = {lat:0, lng:0}
     return (
         <>
             Maps
+            <GoogleMapReact
+            bootstrapURLKeys={{key:''}}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={14}
+            margin={[50,50,50,50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}
+            >
+
+            </GoogleMapReact>
         </>
     )
 }
